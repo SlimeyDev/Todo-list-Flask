f812fd6a4a5b61e03de8ac2a75591edbcf87cca6
SlimeyDev <96341847+SlimeyDev@users.noreply.github.com>
2025-06-24 17:25:25 +0530

    reset

--- todo_list/base/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import taskList
-
-urlpatterns = [
-    path("", taskList.as_view(), name = "tasks"),
-]
--- todo_list/base/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic.list import ListView
-
-def taskList(ListView):
-    model = Task
-
